aabd37c2a0d46b3102e64bee0600db357f08db8d
Viggochang <gl4cj8686@gmail.com>
2021-08-05 10:22:24 +0800

    week-02

--- week-02/web/app.js
+++ week-02/web/app.js
@@ -1,5 +1,16 @@
 const banner = document.querySelector(".banner h2");
+const navIcon = document.querySelector(".nav-icon img");
+const navHidden = document.querySelector(".nav-hidden");
+const navX = document.querySelector(".X")
 
 banner.addEventListener("click", () => {
     banner.textContent = "Have a Good Time!";
-})
+});
+
+navIcon.addEventListener("click", () => {
+    navHidden.getElementsByClassName.display = "flex";
+});
+
+navX.addEventListener("click", () => {
+    navHidden.getElementsByClassName.display = "none";
+});
