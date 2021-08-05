5bbad1e14512dc69c75cbd3cf736ed52c8e67bbf
Viggochang <gl4cj8686@gmail.com>
2021-08-05 11:38:21 +0800

    week-02

--- week-02/web/app.js
+++ week-02/web/app.js
@@ -2,6 +2,9 @@ const banner = document.querySelector(".banner h2");
 const navIcon = document.querySelector(".nav-icon img");
 const navHidden = document.querySelector(".nav-hidden");
 const navX = document.querySelector(".X")
+const actionButton = document.querySelector(".action-button")
+const box56 = document.querySelector(".box-5-6")
+const box78 = document.querySelector(".box-7-8")
 
 banner.addEventListener("click", () => {
     bannerMsg = banner.textContent
@@ -10,14 +13,21 @@ banner.addEventListener("click", () => {
     } else {
         banner.textContent = "Welcome Message";
     }
+    console.log("click bannerMsg");
 });
 
 navIcon.addEventListener("click", () => {
     navHidden.style.display = "flex";
-    console.log("click nav-icon")
+    console.log("click nav-icon");
 });
 
 navX.addEventListener("click", () => {
     navHidden.style.display = "none";
-    console.log("click X")
+    console.log("click X");
+});
+
+actionButton.addEventListener("click", () => {
+    box56.style.display = block;
+    box78.style.display = block;
+    console.log("click actionButton");
 });
