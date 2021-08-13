1d81b5a01f65a8989e7e9e7d2766c155015b2509
Viggochang <gl4cj8686@gmail.com>
2021-08-13 14:04:36 +0800

    week-03

---
+++ week-03/app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/getData", (req, res) => {
+    const { query } = req;
+    res.send(getSum(query));
+});
+
+app.get("/myName", (req, res) => {
+    const { username } = req.cookies;
+    if (username) {
+        res.send(`<h1>Hello ${username}</h1>`);
+    } else {
+        res.redirect("/trackName");
+    }
+});
+
+app.get("/trackName", (req, res) => {
+    const { username } = req.query;
+    if (username) {
+        res.cookie("username", username);
+        res.redirect("/myName");
+    }
+
+    const usernameCookies = req.cookies.username;
+    if (usernameCookies) {
+        res.redirect("/myName");
+    }
+    else {
+        res.render("trackName");
+    }
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`This application is running on localhost:${port}!`);
+});
+
+function sum(num) {
+    let ans = 0;
+    for (let i = 1; i <= num; i++) {
+        ans += i;
+    }
+    return ans;
+};
+
+function getSum(data) {
+    if (Object.keys(data).length === 0) {
+        return "<h1>Lack of Parameter</h1>";
+    } else {
+        const { number } = data;
+        if (!parseInt(number)) {
+            return "<h1>Wong Parameter</h1>";
+        } else {
+            const num = parseInt(number);
+            let ans = sum(num);
+            return `<h1>The answer is ${ans}</h1>`;
+        }
+    }
+};
